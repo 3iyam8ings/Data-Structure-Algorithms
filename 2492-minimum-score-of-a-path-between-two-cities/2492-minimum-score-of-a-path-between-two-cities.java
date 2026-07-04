60f03fe8103bf6a0e8a5a7a03ea64e5e3ce5f828
3iyam8ings <tiyaattechworld@outlook.com>
2026-07-04 12:01:08 +0530

    Time: 8 ms (95.39%), Space: 128.1 MB (94.74%) - LeetHub

--- 2492-minimum-score-of-a-path-between-two-cities/2492-minimum-score-of-a-path-between-two-cities.java
+++ 2492-minimum-score-of-a-path-between-two-cities/2492-minimum-score-of-a-path-between-two-cities.java
@@ -1,41 +1,27 @@
 class Solution {
+    int find(int[] root, int i){
+        if(root[i] == i) return i;
+        return root[i] = find(root, root[i]);
+    }
     public int minScore(int n, int[][] roads) {
+        int[] root = new int[n + 1];
+        for (int i = 0; i <= n; i++)
+            root[i] = i;
+
+         for (int[] r : roads)
+            root[find(root, r[0])] = find(root, r[1]);
+
+               int res = 10001;
+        for (int[] r : roads)
+            if (find(root, r[0]) == find(root, 1))
+                res = Math.min(res, r[2]);
+
+                  return res;
         // 1. create adjancency list of graph
         // 2. perform bfs with city 1
         // 3. maintaine visited array
         // 4. for each road update min edge weight 
         // 5. continue until entire system visited
         // 6. return min edge weight
-
-        List<List<int[]>> adj = new ArrayList<>();
-        for( int i = 0; i<=n ;i++) {
-            adj.add(new ArrayList<>());
-        }
-
-        for(int[] road : roads){
-            int u= road[0];
-            int v = road[1];
-            int w = road[2];
-
-            adj.get(u).add(new int[]{v, w});
-            adj.get(v).add(new int[]{u, w});
-        }
-        boolean[] vis = new boolean[n + 1];
-        Queue<Integer> q = new LinkedList<>();
-        q.offer(1);
-        vis[1] = true;
-        int ans = Integer.MAX_VALUE;
-        while(!q.isEmpty()){
-            int node = q.poll();
-            for(int[] edge : adj.get(node)) {
-                int no = edge[0];
-                int wt = edge[1];
-                ans = Math.min(ans, wt);
-                if(!vis[no]){
-                    vis[no] = true;
-                    q.offer(no);
-                }
-            }
-        } return ans;
     }
 }
