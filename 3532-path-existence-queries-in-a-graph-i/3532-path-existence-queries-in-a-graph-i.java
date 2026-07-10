02f540c08309c3611829609e7b8f897a36f86858
TIYA GARG <tiyaattechworld@outlook.com>
2026-07-10 16:04:24 +0530

    Time: 3 ms (97.09%), Space: 163 MB (74.76%) - LeetHub

--- 3532-path-existence-queries-in-a-graph-i/3532-path-existence-queries-in-a-graph-i.java
+++ 3532-path-existence-queries-in-a-graph-i/3532-path-existence-queries-in-a-graph-i.java
@@ -1,7 +1,9 @@
 class Solution {
     public boolean[] pathExistenceQueries(int n, int[] nums, int maxDiff, int[][] queries) {
+        // connect array having index i
         int[] cmp = new int[n];
         for(int i=1; i<n; i++){
+            // diff b/w 2 consecutive vlues> maxdiff then no path can cross this gap.`
             cmp[i] = cmp[i - 1] + ((nums[i] - nums[i - 1] > maxDiff) ? 1 : 0);
         }
        boolean[] res = new boolean[queries.length];
