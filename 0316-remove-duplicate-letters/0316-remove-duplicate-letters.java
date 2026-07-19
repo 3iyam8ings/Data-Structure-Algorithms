fe2cda1061afb48f97719ee239b5eb20af1e2903
TIYA GARG <tiyaattechworld@outlook.com>
2026-07-19 17:15:09 +0530

    Time: 1 ms (100%), Space: 43.2 MB (98.11%) - LeetHub

---
+++ 0316-remove-duplicate-letters/0316-remove-duplicate-letters.java
@@ -0,0 +1,25 @@
+class Solution {
+    public String removeDuplicateLetters(String s) {
+       int[] last = new int[26];
+       boolean[] seen = new boolean[26] ;
+       //1. store last occurence 
+       for(int i = 0; i<s.length(); i++){
+        last[s.charAt(i) - 'a'] = i;
+       }
+
+       StringBuilder ans = new StringBuilder();
+
+       for(int i=0; i<s.length(); i++){
+        char c = s.charAt(i);
+        if(seen[c -'a']) continue;
+
+        while(ans.length() > 0 && ans.charAt(ans.length() - 1) > c && last[ans.charAt(ans.length() - 1) - 'a'] > i){
+            seen[ans.charAt(ans.length() - 1) -'a'] = false;
+            ans.deleteCharAt(ans.length() - 1);
+        }
+        ans.append(c);
+        seen[c -'a'] = true;
+       }
+       return ans.toString();
+    }
+}
