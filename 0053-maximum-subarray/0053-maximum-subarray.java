62b1c5a110130c13423177c5b3c8cad0aeb068fd
TIYA GARG <tiyaattechworld@outlook.com>
2026-07-20 17:02:18 +0530

    Time: 1 ms (99.96%), Space: 77 MB (92.72%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.java
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -1,12 +1,14 @@
 class Solution {
     public int maxSubArray(int[] nums) {
         //1. start with smallest possible value 
-        int maxSum = Integer.MIN_VALUE;
+        int maxSum = nums[0];
         int curSum = 0;
         for( int num : nums ){
             curSum += num;
         //2. initialise global maximun found fso far 
-            maxSum = Math.max(maxSum, curSum);
+            if( curSum > maxSum) {
+                maxSum = curSum;
+            }
         //3. if sum -ve discard it 
         if( curSum < 0){
             curSum = 0;
