b744f71d3351b31c0f56be0bf1c2ff1ece46a9ba
TIYA GARG <tiyaattechworld@outlook.com>
2026-07-23 17:23:49 +0530

    Time: 0 ms (100%), Space: 125.6 MB (10.2%) - LeetHub

--- 3513-number-of-unique-xor-triplets-i/3513-number-of-unique-xor-triplets-i.java
+++ 3513-number-of-unique-xor-triplets-i/3513-number-of-unique-xor-triplets-i.java
@@ -1,8 +1,8 @@
 class Solution {
     public int uniqueXorTriplets(int[] nums) {
         int n = nums.length;
-
-        int power = (32 - Integer.numberOfLeadingZeros(n) - 3 / (n+1));
-        return 1 << power;
+        if(n < 3) return n;
+        int near = (int)(Math.log(n)/Math.log(2));
+        return 1 << (near + 1);
     }
 }
